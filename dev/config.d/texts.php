41a457df4bf3a4de003026aa40f80005cea1653e
Welington De Lima Olegario <wolegario49@gmail.com>
2025-05-12 14:24:31 -0300

    Configuração dos textos do ambiente

---
+++ dev/config.d/texts.php
@@ -0,0 +1,15 @@
+<?php
+
+use \MapasCulturais\i;
+
+return [
+    "text:home-header.title" => i::__("Bem-vind@!"),
+    "text:home-header.description" => i::__("O Mapa Cacicadas Digitais é uma plataforma livre de confluência entre pessoas e organizações de territórios da Resex Tapajós-Arapiuns. Aqui reunimos dados e informações para uma governança territorial baseada nos nossos saberes."),
+    "text:home-entities.events" => i::__("Quer divulgar ou saber quais atividades estão ocorrendo nas Resex? Festas Tradicionais, Assembléias, Jogos de Futebol! Utilize as ferramentas de busca para encontrar o que está acontecendo. E com cadastro, você também pode incluir suas atividades na plataforma e divulgá-las gratuitamente!"),
+    "text:home-entities.agents" => i::__("Encontre pessoas e organizações dessas comunidades formando uma rede de quem cuida da Floresta. Cadastre-se e inclua também seu perfil e de organizações, coletivos e grupos dos quais faz parte."),
+    "text:home-entities.opportunities" => i::__("Aqui você pode encontrar editais, chamamentos públicos, vagas de trabalho e outras oportunidades que necessitam de inscrições. Além disso, é possível criar seu próprio formulário e divulgar suas oportunidades para outras pessoas, de dentro e de fora das Resex."),
+    "text:home-entities.projects" => i::__("Este é o espaço onde você pode descobrir projetos que estão acontecendo na Resex! Explore, conecte-se e conte o quanto é importante o que acontece por aqui."),
+    "text:home-entities.spaces" => i::__("Procure por lugares que realizam atividades ou ações. Você também pode cadastrar os lugares onde desenvolve suas atividades."),
+    "text:home-feature.description" => i::__("Confira os últimos destaques de cada uma das áreas."),
+    "text:home-register.description" => i::__("Faça seu cadastro e colabore com o Mapa Cacicadas Digitais. Participe desta plataforma livre, colaborativa e interativa de mapeamento na Resex Tapajós-Arapiuns. Esta é uma ferramenta de governança territorial. Se você é de uma Resex ou apoia e contribui com essas comunidades, cadastre-se!"),
+];
