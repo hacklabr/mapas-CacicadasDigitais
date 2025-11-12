f1e359e811aab4d43ec8fea474c7f8b6c19bc8c2
Welington De Lima Olegario <wolegario49@gmail.com>
2025-11-12 17:40:05 -0300

    Adiciona configuração dos plugins

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -6,6 +6,7 @@ return [
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
         'Analytics',
         'Accessibility',
-        'AdminLoginAsUser'
+        'AdminLoginAsUser',
+        'SettingsCacicadas',
     ]
 ];
